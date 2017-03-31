d7bfe5ef8065606e4f9e3bfa51a9e8f62cf29c2c
João <jlribeiroaugusto@gmail.com>
2017-03-31 14:35:32 -0300

    my commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,8 +2,7 @@ var nav  = document.querySelector('nav');
 
 
 window.addEventListener('scroll', function(event){
-	if (document.body.scrollTop > 568){nav.style.opacity = 1;nav.style.transition = 'all .2s linear';}
+	if (document.body.scrollTop > 568){nav.style.opacity = 1; nav.style.transition = 'all .2s linear';}
 	if(document.body.scrollTop < 568)nav.style.opacity = 0;
-
 }, false);
 
